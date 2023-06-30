1215a4677cac32fdcce5b81bb9cf031e18e6eb8e
Zach Berger <zachhberger@gmail.com>
2023-06-29 20:05:07 -0400

    added package.json to server and working on typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,47 @@
 // typeDefs.js
+
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        concerts: [Concert]
+        posts: [Post]
+    }
+
+    type Concert {
+        _id: ID!
+        title: String!
+        date: String!
+        location: String!
+        artist: String!
+        venue: String!
+        city: String!
+        country: String!
+        posts: [Post]
+    }
+
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        photos: [String]
+        videos: [String]
+        user: User!
+        concert: Concert!
+        votes: Int!
+    }
+
+    type Query {
+        users: [User]
+        concerts: [Concert]
+        Posts: [Post]
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): User
+        addPost(title: String!, content: String!, userId: ID!, concertId: ID!): Post
+        votePost(postId: ID!): Post
+    }
