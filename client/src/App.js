8f1d98998a784cca2e6cba6f7c09854d5f3bc21e
Zainab Dabbagh <dabbagh.zainab@gmail.com>
2023-07-03 15:01:52 -0400

    fixed bug and added a build folder for the uploads directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import MediaUpload from "./components/MediaUpload";
 
 
--- server/server.js
+++ server/server.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const express = require('express');
+const path = require("path");
 const searchRouter = require('./routes/searchRouter');
 const app = express()
 PORT = 3001
 
-app.use('/api', searchRouter);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.use(fileUpload())
+app.use('/api', searchRouter, fileUpload());
+
 
 //upload endpoint
 
-app.post('/upload', (req, res) => {
+app.post('/uploads', (req, res) => {
     if(req.files === null) {
-        return res.status(400).json({message: 'no fuele was uploaded'})
+        return res.status(400).json({message: 'no file was uploaded'})
     }
 
     const file =req.files.media
 
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    file.mv(`${__dirname}/../client/build/uploads/${file.name}`, err => {
         if(err) {
             console.error(err)
             return res.status(500).send(err)
@@ -26,6 +29,10 @@ app.post('/upload', (req, res) => {
 
         res.json({ fileName: file.name, filePath: `/uploads/${file.name}`})
     })
+})
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
 })
 
 app.listen(PORT, () => console.log('Serve listening on http://localhost:3001'))
