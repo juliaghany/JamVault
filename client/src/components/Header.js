733a009264c4aa632f7151118a0301ffb1c843dd
Jules <juliaghany@msn.com>
2023-07-07 12:21:37 -0500

    update styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,21 +4,24 @@ import NavBar from './NavBar'
 
 const styles = {
     headerStyle: {
-        backgroundColor: 'black',
-        padding: '30px'
+        // background: 'linear-gradient(45deg, #F23D5E, #BF3480, #A62991, #F26666)',
+        padding: '25px'
     },
     headingStyle: {
         fontSize: '50px',
-        color: 'white',
+        // color: 'white',
+        color: 'black',
         textAlign: 'center'
     },
 }
 
 export default function Header({ currentPage, handlePageChange }) {
     return (
-        <header style={styles.headerStyle}>
-            <h1 style={styles.headingStyle} >JamVault</h1>
+        <div>
             <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
-        </header>
+            {/* <header style={styles.headerStyle}>
+                <h1 style={styles.headingStyle} >JamVault</h1>
+            </header> */}
+        </div>
     );
 }
