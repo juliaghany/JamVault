72049f9f0098b27f718d09318cc32a66d1a9c4f3
Zainab Dabbagh <dabbagh.zainab@gmail.com>
2023-07-08 22:54:31 -0400

    post list code

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,1 +1,24 @@
+import React from "react"
+
+const PostList = ({ posts, media }) => {
+    if (!posts.length) {
+        return <h3>You have no posts</h3>
+    }
+
+    return (
+        <div>
+            <h3>Oh all the places you've been</h3>
+            {posts &&
+            posts.map((post) => (
+                <div key={post.id} className="card mb-3">
+                    <img src={post.media} alt={post.title}/>
+                    <p><small>{post.createdAt}</small></p>
+                </div>
+            ))
+            }
+        </div>
+    )
+}
+
+
 export default PostList
