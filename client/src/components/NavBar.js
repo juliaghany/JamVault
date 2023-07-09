c8afa413781905d01e517e69d90ecef5fd99fdf3
Zainab Dabbagh <dabbagh.zainab@gmail.com>
2023-07-09 14:25:13 -0400

    fixed user posts to only be viewed if user is logged in

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -109,21 +109,3 @@ export default NavBar;
 //                 </a>
 //             </li>
 //         </ul>
-
-
-{/* <div>
-{Auth.loggedIn() ? (
-  <button className="btn btn-lg btn-light m-2" onClick={logout}>
-    Logout
-  </button>
-) : (
-  <>
-    <Link className="btn btn-lg btn-primary m-2" to="/login">
-      Login
-    </Link>
-    <Link className="btn btn-lg btn-light m-2" to="/signup">
-      Signup
-    </Link>
-  </>
-)}
-</div> */}
