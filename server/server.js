0522c731edf86869e736b6332d07095c9e8ab85d
Zainab Dabbagh <dabbagh.zainab@gmail.com>
2023-07-12 09:46:33 -0400

    you can now see pics with posts, gonna go to the corner and cry now

--- server/server.js
+++ server/server.js
@@ -65,9 +65,9 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/uploads', express.static(path.join(__dirname, '../client/build/uploads')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 const startApolloServer = async () => {
     await server.start();
